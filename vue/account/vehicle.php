5e9e8c5ef22f09636723931c394b1e5a4a72cfc0
florianverdonck <florianverdonck@gmail.com>
2016-10-16 13:09:58 +0200

    Test conflit
    Ajout d’un <p> depuis iMac

--- vue/account/vehicle.php
+++ vue/account/vehicle.php
@@ -172,6 +172,7 @@
 						<div id="collapseRDVInstall" class="panel-collapse collapse">
 							<div class="panel-body">
 								<input type="text" value="16-10-2016" class="datepicker">
+								<p> Ceci est ma modification iMac </p>
 							</div>
 						</div>
 					</div>
