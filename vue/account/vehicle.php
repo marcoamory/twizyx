3029f87f683d309b5bbcc0b4a1cddc4b3aa25173
twizyx <info@twizyx.com>
2016-10-16 12:54:01 +0200

    Ajout page vue Vehicule
    Template uniquement, manque la prise de rendez-vous pour l’opération
    d’installation et désinstallation

---
+++ vue/account/vehicle.php
@@ -0,0 +1,353 @@
+<div class="row">
+<div class="col-md-7 col-md-offset-3 col-md-12">
+	<div class="tim-title">
+		<h2>Renault Twizy 45 <span class="label label-default">n° 602</span></h2>
+	</div>
+	<div>
+		<ul class="nav nav-pills tabs">
+			<li class="active" id="orderTab"><a data-toggle="tab">Commande <span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span></a></li>
+			<li id="installTab"><a data-toggle="tab">Installation <span class="glyphicon glyphicon-download" aria-hidden="true"></span></a></li>
+			<li id="uninstallTab"><a data-toggle="tab">Désinstallation</a></li>
+		</ul>
+		<br>
+		
+	</div>
+	
+	<div id="orderTabContent">
+			<p style="font-size: large;"><span class="label label-info" style="font-size: large;">Commande passée le 11 octobre 2016</span>
+			<p>
+			<div id="accordeon">
+				<div class="panel-group" id="accordion">
+					<!-- PREMIER ACCORDEON -->  
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title done">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseCommande">
+								<span id="glyphAccordeon" class="glyphicon glyphicon-list-alt glyphAccordeon" aria-hidden="true"></span>
+								Ma commande
+								</a>
+							</h4>
+						</div>
+						<div id="collapseCommande" class="panel-collapse collapse">
+							<div class="panel-body">
+								<div class="row" style="text-align: center;">
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+										<h6>Véhicule</h6>
+										<span class="label label-warning" style="font-size: 1em;">Twizy 45</span>
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+										<h6>Configuration</h6>
+										<span class="label label-info" style="font-size: 1em;">TwizyX45</span>
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+										<h6>Performances</h6>
+										<span class="label label-success" style="font-size: 1em;">Maximales</span>
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+										<h6>Etat :</h6>
+										<span class="label label-success" style="font-size: 1em;">Installé</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!--  end acordeon 1 -->
+					<!-- SECOND ACCORDEON -->  
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapsePayement">
+								<span id="glyphAccordeon" class="glyphicon glyphicon-euro glyphAccordeon" aria-hidden="true"></span>
+								Payement
+								</a>
+							</h4>
+						</div>
+						<div id="collapsePayement" class="panel-collapse collapse">
+							<div class="panel-body">
+								<div class="row" style="text-align: center;">
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+										<h6>Méthode</h6>
+										<span style="font-size:2em;" class="glyphicon glyphicon-credit-card" aria-hidden="true"></span>
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+										<h6>Prix</h6>
+										<p>€ 299</p>
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+										<h6>Date</h6>
+										<p>19/06/2013</p>
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+										<h6>Télécharger</h6>
+										<span style="font-size:2em;" class="glyphicon glyphicon-save" aria-hidden="true"></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!--  end acordeon 2 -->
+					<!-- TROISIEME ACCORDEON -->  
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseHistorique">
+								<span id="" class="glyphicon glyphicon-th-list glyphAccordeon" aria-hidden="true"></span>
+								Historique du véhicule
+								</a>
+							</h4>
+						</div>
+						<div id="collapseHistorique" class="panel-collapse collapse">
+							<div class="panel-body">
+								<div class="row" style="text-align: center;">
+									<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+										<span class="label label-success" style="font-size: 1em;">Commande</span>
+										<p>19/03/2010</p>
+									</div>
+									<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+										<span class="label label-info" style="font-size: 1em;">Installation</span>
+										<p>En cours...</p>
+									</div>
+									<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+										<span class="label label-default" style="font-size: 1em;">Désinstallation</span>
+										<p></p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!--  end acordeon 3 -->
+				</div>
+				<!--  end of panel group -->
+			</div>
+			<!--  end acordeon -->
+		</div>
+		
+		<div id="installTabContent" class="hidden">
+			<p style="font-size: large;"><span class="label label-info" style="font-size: large;">Installation faite le 11 octobre 2016</span>
+			<p>
+			<div id="accordeon">
+				<div class="panel-group" id="accordion">
+					<!-- PREMIER ACCORDEON -->  
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title done">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseSuiviInstall">
+								<span id="glyphAccordeon" class="glyphicon glyphicon-map-marker glyphAccordeon" aria-hidden="true"></span>
+								Suivi de votre colis
+								</a>
+							</h4>
+						</div>
+						<div id="collapseSuiviInstall" class="panel-collapse collapse">
+							<div class="panel-body">
+								<div class="row" style="text-align: center;">
+									<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+										<h6>Numéro de suivi</h6>
+										<span class="label label-success" style="font-size: 1em;">CE45673930BE</span>
+										
+										<h6>Transporteur</h6>
+										<span class="label label-info" style="font-size: 1em;">BPOST</span>
+									</div>
+									<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+										<h6>Suivi en ligne</h6>
+										<button href="http://www.bpost.be/track" class="btn btn-block btn-lg btn-info btn-fill">
+											<span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>
+											Suivi de mon colis
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!--  end acordeon 1 -->
+					<!-- SECOND ACCORDEON -->  
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseRDVInstall">
+								<span id="glyphAccordeon" class="glyphicon glyphicon-calendar glyphAccordeon" aria-hidden="true"></span>
+								Prise de rendez-vous
+								</a>
+							</h4>
+						</div>
+						<div id="collapseRDVInstall" class="panel-collapse collapse">
+							<div class="panel-body">
+								<input type="text" value="02-16-2012" class="datepicker">
+							</div>
+						</div>
+					</div>
+					<!--  end acordeon 2 -->
+					<!-- TROISIEME ACCORDEON -->  
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapsePrepInstall">
+								<span id="" class="glyphicon glyphicon-wrench glyphAccordeon" aria-hidden="true"></span>
+								Préparez l'installation
+								</a>
+							</h4>
+						</div>
+						<div id="collapsePrepInstall" class="panel-collapse collapse">
+							<div class="panel-body">
+								<div class="alert alert-info fond_jaune">
+							        Afin de gagner du temps et que la procédure de reprogrammation se passe de manière fluide, nous vous invitons à mettre votre véhicule dans les bonnes conditions.
+							        Quelques minutes avant la modifications, veillez à :</div>
+							        
+							        <div class="row">
+										<div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+											<span class="label label-info numeroEtape" style="font-size: 1em;">1</span>
+										</div>
+										<div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+											<p>Vous munir de vos clés et du boîtier TwizyX reçu</p>
+										</div>
+							        </div>
+							        
+							        <div class="row">
+										<div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+											<span class="label label-info numeroEtape" style="font-size: 1em;">2</span>
+										</div>
+										<div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+											<p>Assurez-vous que le véhicule est à l'extérieur et en mesure de capter le réseau téléphone (éloigné des bâtiments). Veuillez également débrancher le cable de recharge.</p>
+										</div>
+							        </div>
+							        
+							        <div class="row">
+										<div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+											<span class="label label-info numeroEtape" style="font-size: 1em;">3</span>
+										</div>
+										<div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+											<p>Repérez le numéro de boîtier inscrit en grand sur celui-ci, il s'agit d'un nombre en deux chiffres écrit en grand sur une des faces du boîtier.</p>
+										</div>
+							        </div>
+							        
+							        <div class="row">
+										<div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+											<span class="label label-info numeroEtape" style="font-size: 1em;">4</span>
+										</div>
+										<div class="col-lg-11 col-md-11 col-sm-11 col-xs-12">
+											<p>Mettez votre téléphone en sonore, nous vous appelons bientôt !</p>
+										</div>
+							        </div>
+							</div>
+						</div>
+					</div>
+					<!--  end acordeon 3 -->
+				</div>
+				<!--  end of panel group -->
+			</div>
+			<!--  end acordeon -->
+		</div>
+		
+		<div id="uninstallTabContent" class="hidden">
+			<p style="font-size: large;"><span class="label label-info" style="font-size: large;">Commande passée le 11 octobre 2016</span>
+			<p>
+						<div id="accordeon">
+				<div class="panel-group" id="accordion">
+					<!-- PREMIER ACCORDEON -->  
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title done">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseSuiviUninstall">
+								<span id="glyphAccordeon" class="glyphicon glyphicon-map-marker glyphAccordeon" aria-hidden="true"></span>
+								Suivi de votre colis
+								</a>
+							</h4>
+						</div>
+						<div id="collapseSuiviUninstall" class="panel-collapse collapse">
+							<div class="panel-body">
+								<div class="row" style="text-align: center;">
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+										<h6>Véhicule</h6>
+										<span class="label label-warning" style="font-size: 1em;">Twizy 45</span>
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+										<h6>Configuration</h6>
+										<span class="label label-info" style="font-size: 1em;">TwizyX45</span>
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+										<h6>Performances</h6>
+										<span class="label label-success" style="font-size: 1em;">Maximales</span>
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+										<h6>Etat :</h6>
+										<span class="label label-success" style="font-size: 1em;">Installé</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!--  end acordeon 1 -->
+					<!-- SECOND ACCORDEON -->  
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseRDVUninstall">
+								<span id="glyphAccordeon" class="glyphicon glyphicon-calendar glyphAccordeon" aria-hidden="true"></span>
+								Prise de rendez-vous
+								</a>
+							</h4>
+						</div>
+						<div id="collapseRDVUninstall" class="panel-collapse collapse">
+							<div class="panel-body">
+								<div class="row" style="text-align: center;">
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+										<h6>Méthode</h6>
+										<span style="font-size:2em;" class="glyphicon glyphicon-credit-card" aria-hidden="true"></span>
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+										<h6>Prix</h6>
+										<p>€ 299</p>
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+										<h6>Date</h6>
+										<p>19/06/2013</p>
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+										<h6>Télécharger</h6>
+										<span style="font-size:2em;" class="glyphicon glyphicon-save" aria-hidden="true"></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!--  end acordeon 2 -->
+					<!-- TROISIEME ACCORDEON -->  
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapsePrepUninstall">
+								<span id="" class="glyphicon glyphicon-wrench glyphAccordeon" aria-hidden="true"></span>
+								Préparez l'installation
+								</a>
+							</h4>
+						</div>
+						<div id="collapsePrepUninstall" class="panel-collapse collapse">
+							<div class="panel-body">
+								<div class="row" style="text-align: center;">
+									<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+										<span class="label label-success" style="font-size: 1em;">Commande</span>
+										<p>19/03/2010</p>
+									</div>
+									<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+										<span class="label label-info" style="font-size: 1em;">Installation</span>
+										<p>En cours...</p>
+									</div>
+									<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+										<span class="label label-default" style="font-size: 1em;">Désinstallation</span>
+										<p></p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!--  end acordeon 3 -->
+				</div>
+				<!--  end of panel group -->
+			</div>
+			<!--  end acordeon -->
+		</div>
+
+
+
+			
+</div>
+</div>
